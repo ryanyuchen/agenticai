010877a9c1037877ba187ef825ac428926b72928
ryanyuchen <vanchen10@gmail.com>
2025-05-18 14:27:55 -0400

    add vector search

---
+++ vector_search/project1/movie_recs.py
@@ -0,0 +1,47 @@
+import pymongo
+import requests
+
+client = pymongo.MongoClient("mongodb+srv://username:token@cluster-genai.nfre6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-GenAI")
+db = client.sample_mflix
+collection = db.movies
+
+items = collection.find().limit(5)
+# for item in items:
+#     print(item)
+
+## Hugging Face Model for Embedding
+# hf_token = 
+embedding_url = "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2"
+
+def generate_embedding(text: str) -> list[float]:
+
+  response = requests.post(
+    embedding_url,
+    headers={"Authorization": f"Bearer {hf_token}"},
+    json={"inputs": text})
+
+  if response.status_code != 200:
+    raise ValueError(f"Request failed with status code {response.status_code}: {response.text}")
+
+  return response.json()
+
+# print(generate_embedding("manchester united is the best"))
+
+# for doc in collection.find({'plot':{"$exists": True}}).limit(50):
+#   doc['plot_embedding_hf'] = generate_embedding(doc['plot'])
+#   collection.replace_one({'_id': doc['_id']}, doc)
+
+query = "imaginary characters from outer space at war"
+
+results = collection.aggregate([
+  {"$vectorSearch": {
+    "queryVector": generate_embedding(query),
+    "path": "plot_embedding_hf",
+    "numCandidates": 100,
+    "limit": 4,
+    "index": "PlotSemanticSearch",
+      }}
+]);
+
+for document in results:
+    print(f'Movie Name: {document["title"]},\nMovie Plot: {document["plot"]}\n')
